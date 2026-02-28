7ca160f785f6b09a0d8e2a968d06c495ea70d454
linuswong <linuswong9@gmail.com>
2026-02-28 11:54:05 -0800

    bug fix

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -4,7 +4,9 @@ import React from 'react';
 import { Platform } from 'react-native';
 
 export function ExternalLink(
-  props: Omit<React.ComponentProps<typeof Link>, 'href'> & { href: string }
+  props: Omit<React.ComponentProps<typeof Link>, 'href'> & {
+    href: Extract<React.ComponentProps<typeof Link>['href'], string>;
+  }
 ) {
   return (
     <Link
@@ -16,7 +18,7 @@ export function ExternalLink(
           // Prevent the default behavior of linking to the default browser on native.
           e.preventDefault();
           // Open the link in an in-app browser.
-          WebBrowser.openBrowserAsync(props.href as string);
+          WebBrowser.openBrowserAsync(props.href);
         }
       }}
     />
